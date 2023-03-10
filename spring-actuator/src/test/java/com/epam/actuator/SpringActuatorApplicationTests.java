56b59a2151d12bc527ede5832cd02d7bf77f7971
Mira <mirabulhakava@gmail.com>
2023-03-10 16:49:53 +0300

    initialized spring actuator

---
+++ spring-actuator/src/test/java/com/epam/actuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
