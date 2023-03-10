dee828df31933c7ac48f973a592b7f8734afb53a
Mira <mirabulhakava@gmail.com>
2023-03-10 16:09:11 +0300

    deleted git folder from custom configuration

---
+++ spring-custom-configuration/src/test/java/com/epam/customconfig/SpringCustomConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.customconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
