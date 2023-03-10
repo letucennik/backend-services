89d53753781164881cf810807d22ebae9315f14a
Mira <mirabulhakava@gmail.com>
2023-03-10 15:34:08 +0300

    deleted git folder in spring auto configuration

---
+++ spring-auto-configuration/src/main/java/com/epam/autoconfig/SpringAutoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.epam.autoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutoConfigurationApplication.class, args);
+    }
+
+}
---
+++ spring-auto-configuration/src/test/java/com/epam/autoconfig/SpringAutoConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.autoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutoConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
