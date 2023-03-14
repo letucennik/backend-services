8b1eec46bec8a150bc639f2e55b7dbe5bc1cedef
Mira <mirabulhakava@gmail.com>
2023-03-14 14:44:46 +0300

    removed unnecessary imports

--- spring-custom-configuration/src/main/java/com/epam/customconfig/config/DataSourceConfig.java
+++ spring-custom-configuration/src/main/java/com/epam/customconfig/config/DataSourceConfig.java
@@ -2,8 +2,6 @@ package com.epam.customconfig.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -11,7 +9,6 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScans(value = {@ComponentScan("com.epam.customconfig.repository")})
 public class DataSourceConfig {
     @Bean
     @ConditionalOnMissingBean
